520b9a6ed7908f70c91fdb73908d35c86856f236
EspiritudeTeflon <101152301+EspiritudeTeflon@users.noreply.github.com>
2022-04-16 14:20:07 -0400

    Add files via upload

---
+++ ejercicio_5.py
@@ -0,0 +1,67 @@
+# Funciones [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.2
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con funciones
+
+# --------------------------------
+# Aquí copiar la función "generar_invitados"
+# ya elaborada
+
+def generar_invitados (cantidad_invitados):
+    lista_de_invitados = []
+
+    for i in range (cantidad_invitados):
+            print ("Ingrese el nombre del invitado", i)
+            invitado = str(input())
+            lista_de_invitados.append(invitado)
+    
+    return lista_de_invitados
+
+# --------------------------------
+
+# --------------------------------
+# Aquí copiar la función "ordenar"
+# ya elaborada
+
+def lista_ordenada( lista_invitados):
+    ordenados= sorted(lista_invitados)
+    return ordenados
+
+# --------------------------------
+
+if __name__ == '__main__':
+    print("Bienvenidos a otra clase de Inove con Python")
+
+    # Alumno: Copiar las funciones "generar_invitados" y "ordenar"
+    # creadas en los ejercicios anteriores
+    # Deberá copiarlas fuera del bucle "__main__" como se detalla
+    # al comienzo del archivo
+    # --> El objetivo es generar una lista de invitados y ordenala
+
+    # Luego de copiar las funciones, invocarla en este lugar:
+
+    # 1) Primero generar una lista de invitados con "generar_invitados"
+    #    Almacenar el resultado en "lista_invitados"
+    cantidad_invitados = 3
+
+    # lista_invitados = generar_invitados()
+    lista_invitados= generar_invitados(cantidad_invitados)
+    print("La lista de invitados es=", lista_invitados)
+
+    # 2) Luego ordenar la lista de invitados con "ordenar"
+    #    --> Pasar como parámetro la "lista_invitados"
+    #    --> Retornar la lista de invitados ordenada
+
+    # lista_invidatos_ordenada = ordenar(lista_invitados)
+
+    lista_invitados_ordenada=lista_ordenada(lista_invitados)
+    # Imprimir en pantalla "lista_invidatos_ordenada":
+    print("la lista de invitados ordenada es=", lista_invitados_ordenada)
+
+    print("terminamos")
